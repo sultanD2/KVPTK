d3ded151015f842351b059df1240e5a857e6c602
sultanD2 <sdarxanbaev@gmail.com>
2025-02-12 18:20:24 +0600

    Add files via upload

---
+++ que.js
@@ -0,0 +1,123 @@
+let questions = [
+	{
+		numb: 1,
+		question: 'Какой тег используется для создания гиперссылки?',
+		answer: 'a',
+		options: [
+			'link',
+			'button',
+			'a',
+			'href'
+		]
+	},	{
+		numb: 2,
+		question: 'Какой тег задаёт заголовок страницы в браузере?',
+		answer: 'title',
+		options: [
+			'header',
+			'title',
+			'h1',
+			'meta'
+		]
+	},	{
+		numb: 3,
+		question: 'Какой тег создаёт нумерованный список?',
+		answer: 'ol',
+		options: [
+			'ul',
+			'ol',
+			'li',
+			'list'
+		]
+	},	{
+		numb: 4,
+		question: 'Какой тег используется для вставки изображения?',
+		answer: 'img',
+		options: [
+			'img',
+			'picture',
+			'src',
+			'icon'
+		]
+	},	{
+		numb: 5,
+		question: 'Какой атрибут указывает путь к изображению в теге img?',
+		answer: 'src',
+		options: [
+			'href',
+			'src',
+			'path',
+			'link'
+		]
+	},	{
+		numb: 6,
+		question: 'Какой тег задаёт основное содержимое страницы?',
+		answer: 'main',
+		options: [
+			'section',
+			'main',
+			'article',
+			'content'
+		]
+	},	{
+		numb: 7,
+		question: 'Какой тег создаёт строку в таблице?',
+		answer: 'tr',
+		options: [
+			'td',
+			'tr',
+			'table',
+			'th'
+		]
+	},	{
+		numb: 8,
+		question: 'Какой тег делает текст жирным?',
+		answer: 'b',
+		options: [
+			'b',
+			'strong',
+			'bold',
+			'thick'
+		]
+	},	{
+		numb: 9,
+		question: 'Какой тег используется для ввода текста пользователем?',
+		answer: 'input',
+		options: [
+			'input',
+			'text',
+			'textarea',
+			'write'
+		]
+	},	{
+		numb: 10,
+		question: 'Какой атрибут делает кнопку отправки формы?',
+		answer: 'type="submit"',
+		options: [
+			'submit',
+			'send',
+			'type="submit"',
+			'action="send"'
+		]
+	}, 	{
+		numb: 11,
+		question: 'Какой тег создаёт раздел в HTML-документе?',
+		answer: 'div',
+		options: [
+			'div',
+			'section',
+			'block',
+			'container'
+		]
+	}, 	{
+		numb: 12,
+		question: 'Какой тег используется для вставки видео?',
+		answer: 'video',
+		options: [
+			'movie',
+			'vid',
+			'video',
+			'media'
+		]
+	}
+]
